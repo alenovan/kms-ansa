5649ad8ae13989dca070bffe47a0ea4af9aa9f93
iskak purnomo <iskak.purnomo11@gmail.com>
2025-01-29 20:22:35 +0700

    feat: checkup age

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -37,3 +37,17 @@ export const calculateAgeInMonths = (birthDate: string) => {
 
     return ageInMonths;
 };
+
+export const calculateAge = (birthDate: string) => {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDifference = today.getMonth() - birthDateObj.getMonth();
+
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDateObj.getDate())) {
+        age--;
+    }
+
+    return age;
+};
