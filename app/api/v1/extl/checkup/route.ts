57ef3b861d24ce939b7454784ed63ddcf344d6bb
alenovanwiradhika <ale@bdn.id>
2025-01-31 13:42:38 +0700

    update

--- app/api/v1/extl/checkup/route.ts
+++ app/api/v1/extl/checkup/route.ts
@@ -66,6 +66,8 @@ export async function POST(request: Request) {
                 data: {
                     name: user.name,
                     status: status.join(', '),
+                    age: calculateAgeInMonths(dateOfBirth),
+                    gender: user.gender,
                 },
             },
             { status: 200 },
